9d93a203607ce1196906270f0a6c4f2d9e6392d9
subhradwip01 <subhradwip01@gmail.com>
2022-10-14 18:43:58 +0530

    BodyPart done ✅
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

---
+++ src/Components/BodyPart.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Stack, Typography } from "@mui/material";
+
+import Icon from "../assets/icons/gym.png";
+
+const BodyPart = ({ item, setBodyPart, bodyPart }) => {
+  return (
+    <Stack
+      type="button"
+      alignItems="center"
+      justifyContent="center"
+      className="body-part-card"
+      sx={{
+        borderTop: bodyPart === item ? "4px solid #ff2625" : "",
+        background: "#fff",
+        borderBottomLeftRadius: "20px",
+        width: "270px",
+        height: "270px",
+        cursor: "pointer",
+        gap: "47px",
+      }}
+      onClick={() => {
+        console.log(item);
+        setBodyPart(item);
+        window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
+      }}
+    >
+      <img src={Icon} alt="gym" style={{ width: "40px", height: "50px" }} />
+      <Typography
+        fontSize="24px"
+        fontWeight="bold"
+        color="#3A1212"
+        textTransform="capitalize"
+      >
+        {item}
+      </Typography>
+    </Stack>
+  );
+};
+
+export default BodyPart;
