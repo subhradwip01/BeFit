e016942d7ebe339180ec99f747714bcc74531cd3
subhradwip01 <subhradwip01@gmail.com>
2022-10-17 13:57:37 +0530

    APP done✅
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,26 @@
-const Home = () =>{
-    return  <div>Home</div>
+import { useState } from "react";
+import { Box } from "@mui/material";
+import HeroBanner from "../Components/HeroBanner";
+import SearchExercises from "../Components/SearchExercises";
+import Exercises from "../Components/Exercises";
+const Home = () => {
+  const [bodyPart, setBodyPart] = useState("all");
+  const [exercises, setExercises] = useState([]);
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises 
+        exercises={exercises}
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+      />
+    </Box>
+  );
+};
 
-}
-
-export default Home
+export default Home;
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,19 +1,35 @@
-export const BASE_URL = "https://exercisedb.p.rapidapi.com"
+export const BASE_URL = "https://exercisedb.p.rapidapi.com";
+export const YOUTUBE_SEARCH_BASE_URL =
+  "https://youtube-search-and-download.p.rapidapi.com";
+
 export const endPoint = {
-    ALL_EXERCISES:"/exercises",
-    BODY_PART_LIST: "/exercises/bodyPartList"
-}
+  ALL_EXERCISES: "/exercises",
+  BODY_PART_LIST: "/exercises/bodyPartList",
+  BODY_PART_EXERCISE: (bodyPartName) => `/exercises/bodyPart/${bodyPartName}`,
+  EXERCISE_DETAILS: (exerciseId) => `/exercises/exercise/${exerciseId}`,
+  TARGET_EXERCISE: (target)=> `/exercises/target/${target}`,
+  EQUIPMENT_EXERCISE: (equipment)=> `/exercises/equipment/${equipment}`,
+  EXERCISE_YOUTUBE_VIDEO: (exerciseName) => `/search?query=${exerciseName}&type=v`
+};
 
 export const fetchDataOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
+};
+
+export const youtubeDataOptions = {
     method: 'GET',
     headers: {
       'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
     }
-  };
-
-export const fetchData = async (url,options)=>{
-    const response = await fetch(url,options);
-    const data = await response.json();
-    return data;
 }
+
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+  return data;
+};
